1022532e658319f2cd53d49542b5b20307f43a75
Nadith12 <nadithjayawardana1@gmail.com>
2025-06-15 12:50:57 +0930

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,58 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Todo List</title>
-</head>
-<body>
-  <h1>ToDoList</h1>
+let List = JSON.parse(localStorage.getItem('todoList')) || [];
 
-  <input type="text" class="input" placeholder="Type here">
-  <button id="button">Add</button>
-  <div class="div"></div>
+document.getElementById('button').onclick = function () {
+  const inputElement = document.querySelector('.input');
+  const inputValue = inputElement.value.trim();
 
-  <script>
-    let List = JSON.parse(localStorage.getItem('todoList')) || [];
-
-    document.getElementById('button').onclick = function () {
-      const inputElement = document.querySelector('.input');
-      const inputValue = inputElement.value.trim();
-
-      if (inputValue !== '') {
-        List.push(inputValue);
-        inputElement.value = '';
-        updateLocalStorage();
-        toDoList();
-      }
-    };
-
-    function toDoList() {
-      let elementHTML = '';
-
-      for (let i = 0; i < List.length; i++) {
-        const Listvalue = List[i];
-        const html =
-          `<p>
-            ${Listvalue}
-            <button onclick="deleteElement(${i});">Delete</button>
-          </p>`;
-        elementHTML += html;
-      }
-
-      document.querySelector('.div').innerHTML = elementHTML;
-    }
-
-    function deleteElement(i) {
-      List.splice(i, 1);
-      updateLocalStorage();
-      toDoList();
-    }
-
-    function updateLocalStorage() {
-      localStorage.setItem('todoList', JSON.stringify(List));
-    }
-
+  if (inputValue !== '') {
+    List.push(inputValue);
+    inputElement.value = '';
+    updateLocalStorage();
     toDoList();
-  </script>
-</body>
-</html>
+  }
+};
+
+function toDoList() {
+  let elementHTML = '';
+
+  for (let i = 0; i < List.length; i++) {
+    const Listvalue = List[i];
+    const html =
+      `<p>
+        ${Listvalue}
+        <button onclick="deleteElement(${i});">Delete</button>
+      </p>`;
+    elementHTML += html;
+  }
+
+  document.querySelector('.div').innerHTML = elementHTML;
+}
+
+function deleteElement(i) {
+  List.splice(i, 1);
+  updateLocalStorage();
+  toDoList();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('todoList', JSON.stringify(List));
+}
+
+// Initial render
+toDoList();
